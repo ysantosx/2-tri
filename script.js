84f9918f51239620117a2fd17d147079986e4057
ysantosx <camila.viante.santos@escola.pr.gov.br>
2026-06-17 08:22:59 -0300

    alteracao

---
+++ script.js
@@ -0,0 +1,31 @@
+const avanca =
+const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+  const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+     })const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+     })const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })<!-- código omitido -->
+
+    <script src="script.js"></script>
